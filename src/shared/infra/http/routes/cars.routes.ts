b2c3000022e411370b40b43901f67b297b490f0b
Glecinilson Silva <glecinilson@gmail.com>
2022-08-03 21:05:22 -0400

    Revert "Adicionando images de carros"
    This reverts commit 0a184dd39ccc0cf38130dcd0b8f7404482961906.

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,11 +1,8 @@
 import { Router } from "express";
-import multer from "multer";
 
-import uploadConfig from "@config/upload";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/listAvailableCarsController";
-import { UploadCarImageController } from "@modules/cars/useCases/uploadCarImage/UploadCarImageController";
 
 import { ensureAdmin } from "../middleware/ensureAdmin";
 import { ensureAuthenticated } from "../middleware/ensureAuthenticated";
@@ -15,9 +12,6 @@ const carsRoutes = Router();
 const createCarController = new CreateCarController();
 const listAvaialableCarsController = new ListAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
-const uploadCarImageController = new UploadCarImageController();
-
-const upload = multer(uploadConfig.upload("./tmp/cars"));
 
 carsRoutes.post(
   "/",
@@ -31,13 +25,6 @@ carsRoutes.post(
   ensureAuthenticated,
   ensureAdmin,
   createCarSpecificationController.handle
-);
-carsRoutes.post(
-  "/images/:id",
-  ensureAuthenticated,
-  ensureAdmin,
-  upload.array("images"),
-  uploadCarImageController.handle
 );
 
 export { carsRoutes };
